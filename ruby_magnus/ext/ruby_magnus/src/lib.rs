229efbd180682f803ec18f150bc7cd153784a176
Kuba Suder <jakub.suder@gmail.com>
2024-04-19 01:51:32 +0200

    added ruby+rust magnus version

---
+++ ruby_magnus/ext/ruby_magnus/src/lib.rs
@@ -0,0 +1,35 @@
+use magnus::{function, Error, Ruby};
+use regex::Regex;
+use lazy_static::lazy_static;
+
+lazy_static! {
+    static ref REGEXPS: Vec<regex::Regex> = vec![
+        Regex::new(r"(?i)linux").unwrap(),
+        Regex::new(r"(?i)debian").unwrap(),
+        Regex::new(r"(?i)ubuntu").unwrap(),
+        Regex::new(r#"\bredhat\b"#).unwrap(),
+        Regex::new(r"\bRHEL\b").unwrap(),
+        Regex::new(r"\bSUSE\b").unwrap(),
+        Regex::new(r"\bCentOS\b").unwrap(),
+        Regex::new(r"(?i)\bopensuse\b").unwrap(),
+        Regex::new(r"(?i)\bslackware\b").unwrap(),
+        Regex::new(r"\bKDE\b").unwrap(),
+        Regex::new(r"\bGTK\d?\b").unwrap(),
+        Regex::new(r"#GNOME\b").unwrap(),
+        Regex::new(r"\bGNOME\s?\d+").unwrap(),
+        Regex::new(r"(?i)\bkde plasma\b").unwrap(),
+        Regex::new(r"apt-get").unwrap(),
+        Regex::new(r"(?i)\bflatpak\b").unwrap(),
+        Regex::new(r"\b[Xx]org\b").unwrap(),
+    ];
+}
+
+fn rust_regex_matches(input_string: String) -> bool {
+    return REGEXPS.iter().any(|r| r.find(&input_string).is_some());
+}
+
+#[magnus::init]
+fn init(ruby: &Ruby) -> Result<(), Error> {
+    ruby.define_global_function("rust_regex_matches", function!(rust_regex_matches, 1));
+    Ok(())
+}
---
+++ ruby_magnus/lib/ruby_magnus.rb
@@ -0,0 +1,1 @@
+require_relative "ruby_magnus/ruby_magnus"
