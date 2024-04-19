dd1cf427608e92a4bf378237601b6c7f1af2b01e
Kuba Suder <jakub.suder@gmail.com>
2024-04-19 03:04:30 +0200

    pass list of regexes from ruby to rust

--- ruby_magnus/ext/ruby_magnus/src/lib.rs
+++ ruby_magnus/ext/ruby_magnus/src/lib.rs
@@ -1,35 +1,30 @@
-use lazy_static::lazy_static;
-use magnus::{function, Error, Ruby};
+use magnus::{function, method, prelude::*, Error, Ruby};
 use regex::Regex;
 
-lazy_static! {
-    static ref REGEXPS: Vec<regex::Regex> = vec![
-        Regex::new(r"(?i)linux").unwrap(),
-        Regex::new(r"(?i)debian").unwrap(),
-        Regex::new(r"(?i)ubuntu").unwrap(),
-        Regex::new(r#"\bredhat\b"#).unwrap(),
-        Regex::new(r"\bRHEL\b").unwrap(),
-        Regex::new(r"\bSUSE\b").unwrap(),
-        Regex::new(r"\bCentOS\b").unwrap(),
-        Regex::new(r"(?i)\bopensuse\b").unwrap(),
-        Regex::new(r"(?i)\bslackware\b").unwrap(),
-        Regex::new(r"\bKDE\b").unwrap(),
-        Regex::new(r"\bGTK\d?\b").unwrap(),
-        Regex::new(r"#GNOME\b").unwrap(),
-        Regex::new(r"\bGNOME\s?\d+").unwrap(),
-        Regex::new(r"(?i)\bkde plasma\b").unwrap(),
-        Regex::new(r"apt-get").unwrap(),
-        Regex::new(r"(?i)\bflatpak\b").unwrap(),
-        Regex::new(r"\b[Xx]org\b").unwrap(),
-    ];
+#[magnus::wrap(class = "RegexMatcher")]
+struct RegexMatcher {
+    regexps: Vec<regex::Regex>,
 }
 
-fn rust_regex_matches(input_string: String) -> bool {
-    return REGEXPS.iter().any(|r| r.find(&input_string).is_some());
+impl RegexMatcher {
+    fn new(patterns: Vec<String>) -> Self {
+        let regexps = patterns
+            .into_iter()
+            .map(|s| Regex::new(&s).unwrap())
+            .collect();
+
+        Self { regexps }
+    }
+
+    fn matches(&self, input_string: String) -> bool {
+        return self.regexps.iter().any(|r| r.find(&input_string).is_some());
+    }
 }
 
 #[magnus::init]
 fn init(ruby: &Ruby) -> Result<(), Error> {
-    ruby.define_global_function("rust_regex_matches", function!(rust_regex_matches, 1));
+    let class = ruby.define_class("RegexMatcher", ruby.class_object())?;
+    class.define_singleton_method("new", function!(RegexMatcher::new, 1))?;
+    class.define_method("matches", method!(RegexMatcher::matches, 1))?;
     Ok(())
 }
