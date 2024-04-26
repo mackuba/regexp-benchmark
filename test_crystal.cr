7bedf2a3e240b210cfb2da5fa7e6a82333c310fc
Kuba Suder <jakub.suder@gmail.com>
2024-04-26 17:01:56 +0200

    run crystal regex with UTF checking disabled

--- test_crystal.cr
+++ test_crystal.cr
@@ -1,21 +1,21 @@
 regexps = [
-  Regex.new("linux", Regex::CompileOptions::IGNORE_CASE),
-  Regex.new("debian", Regex::CompileOptions::IGNORE_CASE),
-  Regex.new("ubuntu", Regex::CompileOptions::IGNORE_CASE),
-  Regex.new("\\bredhat\\b", Regex::CompileOptions::IGNORE_CASE),
-  Regex.new("\\bRHEL\\b"),
-  Regex.new("\\bSUSE\\b"),
-  Regex.new("\\bCentOS\\b"),
-  Regex.new("\\bopensuse\\b", Regex::CompileOptions::IGNORE_CASE),
-  Regex.new("\\bslackware\\b", Regex::CompileOptions::IGNORE_CASE),
-  Regex.new("\\bKDE\\b"),
-  Regex.new("\\bGTK\\d?\\b"),
-  Regex.new("#GNOME\\b"),
-  Regex.new("\\bGNOME\\s?\\d+"),
-  Regex.new("\\bkde plasma\\b", Regex::CompileOptions::IGNORE_CASE),
-  Regex.new("apt\\-get"),
-  Regex.new("\\bflatpak\\b", Regex::CompileOptions::IGNORE_CASE),
-  Regex.new("\\b[Xx]org\\b")
+  Regex.new("linux", Regex::CompileOptions::IGNORE_CASE | Regex::CompileOptions::NO_UTF_CHECK),
+  Regex.new("debian", Regex::CompileOptions::IGNORE_CASE | Regex::CompileOptions::NO_UTF_CHECK),
+  Regex.new("ubuntu", Regex::CompileOptions::IGNORE_CASE | Regex::CompileOptions::NO_UTF_CHECK),
+  Regex.new("\\bredhat\\b", Regex::CompileOptions::IGNORE_CASE | Regex::CompileOptions::NO_UTF_CHECK),
+  Regex.new("\\bRHEL\\b", Regex::CompileOptions::NO_UTF_CHECK),
+  Regex.new("\\bSUSE\\b", Regex::CompileOptions::NO_UTF_CHECK),
+  Regex.new("\\bCentOS\\b", Regex::CompileOptions::NO_UTF_CHECK),
+  Regex.new("\\bopensuse\\b", Regex::CompileOptions::IGNORE_CASE | Regex::CompileOptions::NO_UTF_CHECK),
+  Regex.new("\\bslackware\\b", Regex::CompileOptions::IGNORE_CASE | Regex::CompileOptions::NO_UTF_CHECK),
+  Regex.new("\\bKDE\\b", Regex::CompileOptions::NO_UTF_CHECK),
+  Regex.new("\\bGTK\\d?\\b", Regex::CompileOptions::NO_UTF_CHECK),
+  Regex.new("#GNOME\\b", Regex::CompileOptions::NO_UTF_CHECK),
+  Regex.new("\\bGNOME\\s?\\d+", Regex::CompileOptions::NO_UTF_CHECK),
+  Regex.new("\\bkde plasma\\b", Regex::CompileOptions::IGNORE_CASE | Regex::CompileOptions::NO_UTF_CHECK),
+  Regex.new("apt\\-get", Regex::CompileOptions::NO_UTF_CHECK),
+  Regex.new("\\bflatpak\\b", Regex::CompileOptions::IGNORE_CASE | Regex::CompileOptions::NO_UTF_CHECK),
+  Regex.new("\\b[Xx]org\\b", Regex::CompileOptions::NO_UTF_CHECK)
 ]
 
 posts = File.read_lines("posts.txt")
@@ -25,7 +25,7 @@ matched = 0
 loops.times do
   posts.each do |txt|
     regexps.each do |r|
-      matched += 1 if txt =~ r
+      matched += 1 if r.match(txt, options: Regex::MatchOptions::NO_UTF_CHECK)
     end
   end
 end
