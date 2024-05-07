8bb318afaf94861cc0bd77c31298422f702d4b9f
Kuba Suder <jakub.suder@gmail.com>
2024-05-08 00:36:35 +0200

    hello world versions of rutie and magnus
    to test the library overhead in binary size

--- ruby_magnus/ext/ruby_magnus/src/lib.rs
+++ ruby_magnus/ext/ruby_magnus/src/lib.rs
@@ -1,30 +1,18 @@
-use magnus::{function, method, prelude::*, Error, Ruby};
-use regex::Regex;
+use magnus::{method, prelude::*, Error, Ruby};
 
 #[magnus::wrap(class = "RegexMatcher")]
 struct RegexMatcher {
-    regexps: Vec<regex::Regex>,
 }
 
 impl RegexMatcher {
-    fn new(patterns: Vec<String>) -> Self {
-        let regexps = patterns
-            .into_iter()
-            .map(|s| Regex::new(&s).unwrap())
-            .collect();
-
-        Self { regexps }
-    }
-
-    fn matches(&self, input_string: String) -> bool {
-        return self.regexps.iter().any(|r| r.find(&input_string).is_some());
+    fn hello(&self) -> bool {
+        return true;
     }
 }
 
 #[magnus::init]
 fn init(ruby: &Ruby) -> Result<(), Error> {
     let class = ruby.define_class("RegexMatcher", ruby.class_object())?;
-    class.define_singleton_method("new", function!(RegexMatcher::new, 1))?;
-    class.define_method("matches", method!(RegexMatcher::matches, 1))?;
+    class.define_method("hello", method!(RegexMatcher::hello, 0))?;
     Ok(())
 }
--- ruby_rutie/src/lib.rs
+++ ruby_rutie/src/lib.rs
@@ -1,44 +1,15 @@
 #[macro_use]
 extern crate rutie;
 
-use lazy_static::lazy_static;
-use regex::Regex;
-use rutie::{Boolean, Class, Object, RString, VM};
-
-lazy_static! {
-    static ref REGEXPS: Vec<regex::Regex> = vec![
-        Regex::new(r"(?i)linux").unwrap(),
-        Regex::new(r"(?i)debian").unwrap(),
-        Regex::new(r"(?i)ubuntu").unwrap(),
-        Regex::new(r#"\bredhat\b"#).unwrap(),
-        Regex::new(r"\bRHEL\b").unwrap(),
-        Regex::new(r"\bSUSE\b").unwrap(),
-        Regex::new(r"\bCentOS\b").unwrap(),
-        Regex::new(r"(?i)\bopensuse\b").unwrap(),
-        Regex::new(r"(?i)\bslackware\b").unwrap(),
-        Regex::new(r"\bKDE\b").unwrap(),
-        Regex::new(r"\bGTK\d?\b").unwrap(),
-        Regex::new(r"#GNOME\b").unwrap(),
-        Regex::new(r"\bGNOME\s?\d+").unwrap(),
-        Regex::new(r"(?i)\bkde plasma\b").unwrap(),
-        Regex::new(r"apt-get").unwrap(),
-        Regex::new(r"(?i)\bflatpak\b").unwrap(),
-        Regex::new(r"\b[Xx]org\b").unwrap(),
-    ];
-}
+use rutie::{Boolean, Class, Object};
 
 class!(RubyRustBridge);
 
 methods!(
     RubyRustBridge,
     _rtself,
-    fn regex_matches(input: RString) -> Boolean {
-        let ruby_string = input.map_err(|e| VM::raise_ex(e)).unwrap();
-
-        let string = &ruby_string.to_string();
-
-        let matches = REGEXPS.iter().any(|r| r.find(&string).is_some());
-        return Boolean::new(matches);
+    fn hello() -> Boolean {
+        return Boolean::new(true);
     }
 );
 
@@ -46,6 +17,6 @@ methods!(
 #[no_mangle]
 pub extern "C" fn Init_rutie_ruby() {
     Class::new("RubyRustBridge", None).define(|klass| {
-        klass.def_self("regex_matches", regex_matches);
+        klass.def_self("hello", hello);
     });
 }
